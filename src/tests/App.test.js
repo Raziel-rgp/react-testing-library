df06ba53ff13a7a2fe3217ad8ce706eec91df855
fawl_03 <razidias@gmail.com>
2022-09-05 19:17:51 -0300

    att 0.0.1 testando o stryker

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderPath from './helpers/renderPath';
+
+describe('1- Será validado se o cabeçario está funcional ', () => {
+  it('Será verificado se na pagina inicial é renderizada corretamente', () => {
+    renderPath('/');
+
+    expect(screen.getByText('Encountered pokémons')).toBeInTheDocument();
+  });
+  it('Será verificado se a pagina "about" é renderizada corretamente', () => {
+    renderPath('/about');
+
+    expect(screen.getByText('About Pokédex')).toBeInTheDocument();
+  });
+  it('Será verificado se a padina "Favorite Pokémons" é renderizada corretamente', () => {
+    renderPath('/favorites');
+
+    expect(screen.getByText('Favorite pokémons')).toBeInTheDocument();
+  });
+});
