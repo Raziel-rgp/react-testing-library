10285881521246805ac693515c7a9cf9dd4f54dd
fawl_03 <razidias@gmail.com>
2022-09-06 18:27:35 -0300

    att 1.1.0

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,20 +1,20 @@
+import React from 'react';
 import { screen } from '@testing-library/react';
-import renderPath from './helpers/renderPath';
+import App from '../App';
+import renderWithRouter from './helpers/renderPath';
 
 describe('1- Será validado se o cabeçario está funcional ', () => {
-  it('Será verificado se na pagina inicial é renderizada corretamente', () => {
-    renderPath('/');
-
-    expect(screen.getByText('Encountered pokémons')).toBeInTheDocument();
+  test('Será verificado se na pagina inicial é renderizada corretamente', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByRole('link', { name: 'Home' })).toBeInTheDocument();
   });
-  it('Será verificado se a pagina "about" é renderizada corretamente', () => {
-    renderPath('/about');
-
-    expect(screen.getByText('About Pokédex')).toBeInTheDocument();
+  test('Será verificado se a pagina "about" é renderizada corretamente', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByRole('link', { name: 'About' })).toBeInTheDocument();
   });
-  it('Será verificado se a padina "Favorite Pokémons" é renderizada corretamente', () => {
-    renderPath('/favorites');
-
-    expect(screen.getByText('Favorite pokémons')).toBeInTheDocument();
+  test('Será verificado se a padina "Favorite Pokémons" é render corretamente', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByRole('link',
+      { name: /Favorite Pokémons/i })).toBeInTheDocument();
   });
 });
