6daea2af3e73bea526569c55f098e4f7ec290552
fawl_03 <razidias@gmail.com>
2022-09-08 00:11:00 -0300

    att 1.5.5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
@@ -11,4 +11,4 @@ describe('', () => {
   test('', () => {
     
   });
-});
+}); */
